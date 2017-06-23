fix to categorical output layer use of softmax other minor refactors

tensor_dynamic/layers/output_layer.py
@@ -1,10 +1,9 @@
 import logging
-
 import math
 
-from tensor_dynamic.layers.layer import Layer
 import tensorflow as tf
 
+from tensor_dynamic.layers.layer import Layer
 from tensor_dynamic.lazyprop import lazyprop
 from tensor_dynamic.tf_loss_functions import squared_loss
 from tensor_dynamic.utils import get_tf_optimizer_variables, train_till_convergence
@@ -106,7 +105,9 @@ class OutputLayer(Layer):
 
     def train_till_convergence(self, data_set_train, data_set_validation=None, mini_batch_size=100,
                                continue_epochs=2, learning_rate=0.0001,
-                               optimizer=tf.train.AdamOptimizer):
+                               optimizer=tf.train.AdamOptimizer,
+                               on_iteration_complete_func=None,
+                               on_mini_batch_complete_func=None):
         """Train this network until we stopping seeing an improvement in the error of the validation set
 
         Args:
@@ -121,11 +122,11 @@ class OutputLayer(Layer):
         Returns:
             float: Error/Accuracy we finally converged on
         """
-        optimizer_instance = tf.train.RMSPropOptimizer(learning_rate,
-                                              name="prop_for_%s" % (str(self.get_resizable_dimension_size_all_layers())
-                                                                    .replace('(', '_').replace(')', '_')
-                                                                    .replace('[', '_').replace(']', '_')
-                                                                    .replace(',', '_').replace(' ', '_'),))
+        optimizer_instance = optimizer(learning_rate,
+                                       name="prop_for_%s" % (str(self.get_resizable_dimension_size_all_layers())
+                                                             .replace('(', '_').replace(')', '_')
+                                                             .replace('[', '_').replace(']', '_')
+                                                             .replace(',', '_').replace(' ', '_'),))
         train_op = optimizer_instance.minimize(self.loss_op_train)
 
         self._session.run(tf.initialize_variables(list(get_tf_optimizer_variables(optimizer_instance))))
@@ -161,6 +162,9 @@ class OutputLayer(Layer):
 
                 print(error, acc / parts)
 
+            if on_iteration_complete_func is not None:
+                on_iteration_complete_func(self, iterations[0])
+
             return error
 
         error = train_till_convergence(train, log=False, continue_epochs=continue_epochs)
@@ -188,7 +192,7 @@ class OutputLayer(Layer):
         return log_prob, accuracy, target_loss
 
 
-class BinaryLayer(OutputLayer):
+class BinaryOutputLayer(OutputLayer):
     def __init__(self, input_layer,
                  session=None,
                  bias=None,
@@ -199,16 +203,16 @@ class BinaryLayer(OutputLayer):
                  noise_std=None,
                  regularizer_weighting=0.01,
                  name='BinaryOutputLayer'):
-        super(BinaryLayer, self).__init__(input_layer, (1,),
-                                                     session=session,
-                                                     bias=bias,
-                                                     weights=weights,
-                                                     back_bias=back_bias,
-                                                     freeze=freeze,
-                                                     weight_extender_func=weight_extender_func,
-                                                     noise_std=noise_std,
-                                                     regularizer_weighting=regularizer_weighting,
-                                                     name=name)
+        super(BinaryOutputLayer, self).__init__(input_layer, (1,),
+                                                session=session,
+                                                bias=bias,
+                                                weights=weights,
+                                                back_bias=back_bias,
+                                                freeze=freeze,
+                                                weight_extender_func=weight_extender_func,
+                                                noise_std=noise_std,
+                                                regularizer_weighting=regularizer_weighting,
+                                                name=name)
 
     @lazyprop
     def accuracy_op(self):
@@ -219,54 +223,3 @@ class BinaryLayer(OutputLayer):
                                       tf.float32))
 
 
-class CategoricalOutputLayer(OutputLayer):
-    def __init__(self, input_layer, output_nodes,
-                 session=None,
-                 bias=None,
-                 weights=None,
-                 back_bias=None,
-                 freeze=False,
-                 weight_extender_func=noise_weight_extender,
-                 noise_std=None,
-                 regularizer_weighting=0.01,
-                 name='CategoricalOutputLayer'):
-        super(CategoricalOutputLayer, self).__init__(input_layer, output_nodes,
-                                                     session=session,
-                                                     bias=bias,
-                                                     weights=weights,
-                                                     back_bias=back_bias,
-                                                     freeze=freeze,
-                                                     weight_extender_func=weight_extender_func,
-                                                     noise_std=noise_std,
-                                                     regularizer_weighting=regularizer_weighting,
-                                                     name=name)
-
-    def _layer_activation(self, input_activation, is_train):
-        return tf.nn.softmax(tf.matmul(input_activation, self._weights) + self._bias)
-
-    def _target_loss_op(self, input_tensor):
-        return tf.reduce_sum(
-            tf.nn.softmax_cross_entropy_with_logits(logits=input_tensor, labels=self._target_placeholder),
-        )
-
-    @lazyprop
-    def accuracy_op(self):
-        return tf.reduce_mean(tf.cast(tf.nn.in_top_k(self.activation_predict, tf.argmax(self.target_placeholder, 1), 1),
-                                      tf.float32))
-
-    def accuracy(self, inputs, targets):
-        """Get the accuracy of our predictions against the real targets, returns a value in the range 0. to 1.
-
-        Args:
-            inputs (np.array):
-            targets (np.array):
-
-        Returns:
-            float: accuracy in the range 0. to 1.
-        """
-        return self.session.run(self.accuracy_op,
-                                feed_dict={self.input_placeholder: inputs, self._target_placeholder: targets})
-
-    @lazyprop
-    def log_probability_of_targets_op(self):
-        return tf.reduce_sum(tf.log(tf.reduce_sum(self.activation_predict * self.target_placeholder, 1)))

tests/experiments/grid_search.py
@@ -18,7 +18,9 @@ def do_grid_search(data_set_collection, model_functions, file_name, learning_rat
         extra_parameters (dict):
         learning_rate (float):
     """
-    write_parameters_file(data_set_collection, extra_parameters, file_name, learning_rate, continue_epochs)
+    extra_parameters['learning_rate'] = learning_rate
+    extra_parameters['continue_epochs'] = continue_epochs
+    write_parameters_file(data_set_collection, file_name, **extra_parameters)
     with open(file_name, 'w') as result_file:
         result_file.write(
             'log_prob_train, error_train, accuracy_train, error_test, accuracy_test, log_prob_test, dimensions, parameters\n')
@@ -43,13 +45,11 @@ def do_grid_search(data_set_collection, model_functions, file_name, learning_rat
                                                                  model.get_parameters_all_layers()))
 
 
-def write_parameters_file(data_set_collection, extra_parameters, file_name, learning_rate, continue_epochs):
+def write_parameters_file(data_set_collection, file_name, **kwargs):
     with open(file_name + '.txt', 'w') as param_file:
         param_file.write("data_set=%s\n" % (data_set_collection.name,))
         param_file.write("data_set_normalized=%s\n" % (data_set_collection.normlized,))
-        param_file.write("learning_rate=%s\n" % (learning_rate,))
-        param_file.write("continue_epochs=%s\n" % (continue_epochs,))
-        for key, value in extra_parameters.iteritems():
+        for key, value in kwargs.iteritems():
             param_file.write("%s=%s\n" % (key, value))
 
 
@@ -74,18 +74,18 @@ def flat_model_functions(data_set_collection, regularizer, activation_func, use_
 
 
 if __name__ == '__main__':
-    regularizer = 0.0
+    regularizer = 0.01
 
     data_set_collection = get_mnist_data()
-    use_noisy_input_layer = True
+    use_noisy_input_layer = False
     do_grid_search(data_set_collection,
                    functools.partial(flat_model_functions,
                                      regularizer=regularizer,
                                      activation_func=tf.nn.relu,
                                      use_noisy_input_layer=use_noisy_input_layer),
-                   'flat_mnist_grid_search_no_reg.csv',
+                   'flat_adam_reg_softmax_fix.csv',
                    regularizer=regularizer,
                    use_noisy_input_layer=use_noisy_input_layer,
-                   activation_func='relu',
+                   activation_func=tf.nn.relu,
                    network='flat',
                    optimizer=tf.train.AdamOptimizer)