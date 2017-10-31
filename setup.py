0641bc601f77f8fcc353cd9a7fa72174b4848b6d
daniel <daniel.slater@skimlinks.com>
2017-10-31 18:39:40 +0000

    first version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tensor-dynamic',
-    version='0.0.1-SNAPSHOT',
+    version='0.0.1',
     author='Daniel Slater',
     description='Library for dynamicly learning network topology',
     url='https://github.com/DanielSlater/tensordynamic',
